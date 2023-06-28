939519a67b2ae51272765391724fffb53ac89a87
TusharAppinventiv <137896087+TusharAppinventiv@users.noreply.github.com>
2023-06-28 18:19:22 +0530

    while loop in JS

---
+++ while_loop.js
@@ -0,0 +1,12 @@
+//program to add all digits upto specific digit
+let num = prompt("Enter a number :");
+let i = 0;
+let ans = 0;
+let upd = 0;
+//While loops can be single-lined or contain multiple commands for a single condition.
+while(i <= num){
+    ans = ans + upd;
+    upd++;
+    i++;
+}
+console.log(ans);
