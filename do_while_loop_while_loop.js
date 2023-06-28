e1835cd1fd097e6f6c35c1f054501f97db069d38
TusharAppinventiv <tushar.appinventiv.com>
2023-06-28 23:21:44 +0530

    while_do_while

---
+++ do_while_loop_while_loop.js
@@ -0,0 +1,20 @@
+//program to add all digits upto specific digit
+let num = prompt("Enter a number :");
+let i = 0;
+let ans = 0;
+let upd = 0;
+//While loops can be single-lined or contain multiple commands for a single condition.
+while(i <= num){
+    ans = ans + upd;
+    upd++;
+    i++;
+}
+console.log(ans);
+//do-while loops gets executed at least once as the condition check is present in the end
+let out = 0;
+let j = 0;
+do {
+  out += j;
+  j++;
+}
+while (ij< 5);
