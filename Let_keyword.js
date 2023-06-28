c91cffd13b7a768b49afc2c19591cf4ddcd1ede4
TusharAppinventiv <137896087+TusharAppinventiv@users.noreply.github.com>
2023-06-28 18:00:27 +0530

    successfully commited

---
+++ Let_keyword.js
@@ -0,0 +1,3 @@
+let age = 22; // let can automatically assign the data type accordingly it would detect it's a number
+let name = "Tushar"; // It would automatically detect the given data is string
+console.log("My name is :", name , "my age is :", age);
