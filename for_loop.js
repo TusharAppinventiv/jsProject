ebfe313cca13f05c6ce23bdf305d1b4632410336
TusharAppinventiv <137896087+TusharAppinventiv@users.noreply.github.com>
2023-06-28 18:09:12 +0530

    for loop in JS

---
+++ for_loop.js
@@ -0,0 +1,6 @@
+let num = prompt("Enter a number :");
+for(let i = 1 ; i <= 10 ; i++){
+    //for loop is a single-line command that will be executed repeatedly
+    let n = num * i;
+    console.log(num, "*", i , "=", n);
+}
