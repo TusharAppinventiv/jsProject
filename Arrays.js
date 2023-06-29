34b01e2673c7862d368c01081bda030b7f5903f3
TusharAppinventiv <tushar.appinventiv.com>
2023-06-29 10:13:12 +0530

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -13,3 +13,21 @@ const Arr = num.sort(function(a, b){return a-b});
 for(let i = 0 ; i < num.length ; i++){
     console.log(Arr[i]);
 }
+
+const array1 = ['Tushar', '+', 'Vatsa'];
+array1.forEach(element => console.log(element));
+// The forEach() method executes a provided function once for each array element.
+
+//The reduce() method reduces an array of values down to just one value. To get the output value, 
+//it runs a reducer function on each element of the array and give us the ouutput.
+const numbers = [1, 2, 3, 4];
+const multiply = numbers.reduce(function (result, item) {
+  return multiply * item;
+}, 0);
+console.log(sum); // 10
+
+//The filter() method takes each element in an array and it applies a conditional statement against it. If this conditional returns true, the element gets pushed to the output array.
+// If the condition returns false, the element does not get pushed to the output array.
+const nume= [1, 2, 3, 4];
+const odds = nume.filter(item => item % 2 != 0);
+console.log(odds); 
