c557e3c88f20cae4b149c4f4f62392bb8f133c91
TusharAppinventiv <tushar.appinventiv.com>
2023-06-30 10:13:57 +0530

    Commited

--- JS_features.js
+++ JS_features.js
@@ -1,9 +1,9 @@
-//This code is explaining how Javascript is Single Threaded, Non - Blocking, Dynamic Typed
+//This code is explaining how Javascript is Single Threaded, Non - Blocking, Dynamic Typed, Event driven
 let a = "Tushar";
 let b = 24;
 let c = "Vatsa"
 console.log(a);
 setTimeout(() => {
-  console.log(b);;
+  console.log(b);
 }, 1500);
-console.log(c)
+console.log(c);
