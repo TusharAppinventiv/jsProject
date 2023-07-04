be4f8cbd1444a19742ef7b30f274bc889a0c52af
prashantappinventiv <137900157+prashantappinventiv@users.noreply.github.com>
2023-07-04 17:25:01 +0530

    Update table.js
    i add a line console.log();

--- table.js
+++ table.js
@@ -4,7 +4,8 @@ const number = parseInt(prompt('Enter an integer: '));
 for(let i = 1; i <= 10; i++) {
 
 
-    const result = i * number;
+    const result = i * number;]
+    console.log("i performed changes");
 
 
     console.log(`${number} * ${i} = ${result}`);
